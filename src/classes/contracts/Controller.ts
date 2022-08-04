ec9f5ffad01c5dc3eafe80bcb615eb25b917d2e8
findolor <anakisci@gmail.com>
2022-08-04 23:36:24 +0300

    Refactor controller contract

--- src/classes/contracts/Controller.ts
+++ src/classes/contracts/Controller.ts
@@ -11,7 +11,9 @@ import { MODULES } from '../../types'
 import {
   InstantiateMsg,
   InitModuleMsg,
-  InitMarketplaceModuleMsg
+  InitMarketplaceModuleMsg,
+  ExecuteMsg,
+  QueryMsg
 } from '../../types/contracts/controller'
 
 const CODE_ID = 1
@@ -35,31 +37,37 @@ export class ControllerContract extends ContractWrapper {
     )
   }
 
-  async initMintModule(msg: InitModuleMsg): Promise<ExecuteResult> {
-    return super.execute(msg, 'auto')
+  async initMintModule({ code_id }: InitModuleMsg): Promise<ExecuteResult> {
+    return super.execute({ [`${ExecuteMsg.INIT_MINT_MODULE}`]: { code_id } }, 'auto')
   }
 
-  async initMergeModule(msg: InitModuleMsg): Promise<ExecuteResult> {
-    return super.execute(msg, 'auto')
+  async initMergeModule({ code_id }: InitModuleMsg): Promise<ExecuteResult> {
+    return super.execute({ [`${ExecuteMsg.INIT_MERGE_MODULE}`]: { code_id } }, 'auto')
   }
 
-  async initPermissionModule(msg: InitModuleMsg): Promise<ExecuteResult> {
-    return super.execute(msg, 'auto')
+  async initPermissionModule({ code_id }: InitModuleMsg): Promise<ExecuteResult> {
+    return super.execute({ [`${ExecuteMsg.INIT_PERMISSION_MODULE}`]: { code_id } }, 'auto')
   }
 
-  async initMarketplaceModule(msg: InitMarketplaceModuleMsg): Promise<ExecuteResult> {
-    return super.execute(msg, 'auto')
+  async initMarketplaceModule({
+    code_id,
+    native_denom
+  }: InitMarketplaceModuleMsg): Promise<ExecuteResult> {
+    return super.execute(
+      { [`${ExecuteMsg.INIT_MARKETPLACE_MODULE}`]: { code_id, native_denom } },
+      'auto'
+    )
   }
 
   async getConfig(): Promise<any> {
-    return super.query({ config: {} })
+    return super.query({ [`${QueryMsg.CONFIG}`]: {} })
   }
 
   async getControllerInfo(): Promise<any> {
-    return super.query({ controller_info: {} })
+    return super.query({ [`${QueryMsg.CONTROLLER_INFO}`]: {} })
   }
 
   async getModuleAddress(module: MODULES): Promise<any> {
-    return super.query({ module_address: module })
+    return super.query({ [`${QueryMsg.MODULE_ADDRESS}`]: module })
   }
 }
--- src/types/contracts/controller.ts
+++ src/types/contracts/controller.ts
@@ -4,7 +4,20 @@ export type ExecuteMsg =
   | 'init_merge_module'
   | 'init_marketplace_module'
 
+export const ExecuteMsg = {
+  INIT_MINT_MODULE: 'init_mint_module',
+  INIT_PERMISSION_MODULE: 'init_permission_module',
+  INIT_MERGE_MODULE: 'init_merge_module',
+  INIT_MARKETPLACE_MODULE: 'init_marketplace_module'
+}
+
 export type QueryMsg = 'config' | 'controller_info' | 'module_address'
+
+export const QueryMsg = {
+  CONFIG: 'config',
+  CONTROLLER_INFO: 'controller_info',
+  MODULE_ADDRESS: 'module_address'
+}
 
 export interface InstantiateMsg {
   name: string
