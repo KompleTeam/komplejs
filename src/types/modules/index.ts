e60cabf3f64abfd712ea1d9195b8147bbb3ab61e
findolor <anakisci@gmail.com>
2022-08-11 14:50:28 +0300

    Update module type

--- src/types/modules/index.ts
+++ src/types/modules/index.ts
@@ -3,11 +3,11 @@ export * from './merge'
 export * from './permission'
 export * from './marketplace'
 
-export type MODULES = 'mint' | 'merge' | 'permission' | 'marketplace'
+export type MODULES = 'mint_module' | 'merge_module' | 'permission_module' | 'marketplace_module'
 
 export const MODULES = {
-  MINT: 'mint' as MODULES,
-  MERGE: 'merge' as MODULES,
-  PERMISSION: 'permission' as MODULES,
-  MARKETPLACE: 'marketplace' as MODULES
+  MINT: 'mint_module' as MODULES,
+  MERGE: 'merge_module' as MODULES,
+  PERMISSION: 'permission_module' as MODULES,
+  MARKETPLACE: 'marketplace_module' as MODULES
 }
